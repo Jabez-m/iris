f0afc3e4785c3bd764c47eae484e52ec53a6ee96
Jabez Modichai <106453288+Jabez-m@users.noreply.github.com>
2022-11-20 00:43:42 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+model=pickle.load(open('pik.pkl','rb'))
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods='POST')
+def predict():
+    f=[float(x) for x in request.form.values()]
+    features=[np.array(f)]
+    prediction=model.predict(features)
+    return render_template('index.html',pred=''.format(prediction))
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
